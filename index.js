8495a792528d68a27e8a18cbaf0a3b767b5bebb4
Lakshya Gupta <lakshya@thesilverlabs.com>
2020-07-07 17:13:46 +0530

    Add more routes

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ let db;
 
 const connect = () => new Promise((resolve, reject) => {
 	if (db) return resolve(db)
-	const client = new MongoClient('mongodb://11.0.1.139:27017', { useNewUrlParser: true })
+	const client = new MongoClient('mongodb://localhost:27017', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		compression: ['snappy']
+	})
 	client.connect((err) => {
 		if (err) return reject(err)
 		db = client.db('rumbl')
@@ -17,8 +21,6 @@ const connect = () => new Promise((resolve, reject) => {
 connect().then(() => 'Connected to mongodb')
 
 const app = express();
-
-app.use(compression());
 
 app.get('/health', (req, res) => res.end('healthy'));
 
@@ -32,6 +34,62 @@ app.get('/test', async (req, res) => {
 	res.json(posts);
 });
 
+app.get('/write-one', async (req, res) => {
+	await db.collection('test_collection').insertOne({ test: 1 });
+	res.json({ success: true });
+});
+
+app.get('/write-ten', async (req, res) => {
+	const docs = Array(10).fill().map((val, index) => ({ test: index }))
+	await db.collection('test_collection').insertMany(docs);
+	res.json({ success: true });
+});
+
+app.get('/read-one', async (req, res) => {
+	const docs = await db.collection('test_collection')
+		.find({}, { readPreference: 'secondaryPreferred' })
+		.sort({ _id: -1 })
+		.limit(1)
+		.toArray();
+	res.json(docs);
+});
+
+app.get('/read-ten', async (req, res) => {
+	const docs = await db.collection('test_collection')
+		.find({}, { readPreference: 'secondaryPreferred' })
+		.sort({ _id: -1 })
+		.limit(10)
+		.toArray();
+	res.json(docs);
+});
+
+app.get('/update-two-read-ten', async ({ req, res }) => {
+	const docs = await db.collection('test_collection')
+		.find({}, { readPreference: 'secondaryPreferred' })
+		.sort({ _id: -1 })
+		.limit(10)
+		.toArray();
+	const idsToUpdate = docs.slice(0, 2).map(doc => doc._id);
+	await db.collection('test_collection').updateMany(
+		{ _id: { $in: idsToUpdate } },
+		{ $set: { test: 0 } },
+	)
+	res.json(docs);
+});
+
+app.get('/insert-two-read-ten', async ({ req, res }) => {
+	const toInsert = Array(2).fill().map((val, index) => ({ test: index }))
+	await db.collection('test_collection').insertMany(toInsert);
+
+	const docs = await db.collection('test_collection')
+		.find({}, { readPreference: 'secondaryPreferred' })
+		.sort({ _id: -1 })
+		.limit(10)
+		.toArray();
+
+	res.json(docs);
+});
+
 app.listen(5000, () => {
 	console.log('App Listening');
-})
+});
